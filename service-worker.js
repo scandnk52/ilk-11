3a9065d87cf59d6d2c3ebb692dfb01e244044eb8
Samet Can <scandnk52@gmail.com>
2024-02-27 17:05:59 +0300

    v3.10

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,51 @@
-self.addEventListener('install', function () {
-    console.log('Install!');
-});
+var APP_PREFIX = 'İLK 11'
+var VERSION = 'v3.1'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+    '/ilk-11/',
+    '/ilk-11/index.html'
+]
 
-self.addEventListener("activate", event => {
-    console.log('Activate!');
-});
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+        if (request) {
+            console.log('responding with cache : ' + e.request.url)
+            return request
+        } else { 
+            console.log('file is not cached, fetching : ' + e.request.url)
+            return fetch(e.request)
+        }
+    })
+    )
+})
 
-self.addEventListener('fetch', function (event) {
-    console.log('Fetch!', event.request);
-});
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(URLS)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+
+            var cacheWhitelist = keyList.filter(function (key) {
+            return key.indexOf(APP_PREFIX)
+            })
+
+            cacheWhitelist.push(CACHE_NAME)
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                console.log('deleting cache : ' + keyList[i] )
+                return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
